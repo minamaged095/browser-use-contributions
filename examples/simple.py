39121faefab4b136b4d5df87eeeecf59d6bd37d1
Mina Maged <minamaged095@gmail.com>
2025-04-28 14:06:29 +0300

    restore simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -12,22 +12,13 @@ from browser_use import Agent
 
 load_dotenv()
 
-def cli_human_help_handler(msg: str, inst: str) -> str:
-    prompt = msg
-    if inst:
-        prompt += "\n" + inst
-    prompt += "\n> "  # terminal prompt
-    return input(prompt).strip()
-
-
 # Initialize the model
 llm = ChatOpenAI(
 	model='gpt-4o',
 	temperature=0.0,
 )
-task = 'ask for human help then Go to kayak.com and find the cheapest flight from Zurich to San Francisco on 2025-05-01'
+task = 'Go to kayak.com and find the cheapest flight from Zurich to San Francisco on 2025-05-01'
 
-# agent = Agent(task=task, llm=llm, allow_yield_to_human=True, human_help_callback=cli_human_help_handler)
 agent = Agent(task=task, llm=llm)
 
 
